df7ba7a9c6eb71e58b8903932a75a8e423861ca1
Dewyn <dewyn.liriano@gmail.com>
2017-12-04 20:19:32 -0400

    Initial Commit.

--- facturacion/models.py
+++ facturacion/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Factura(models.Model):
+    nombre = models.CharField(max_length=20, null=False)
+    monto = models.DecimalField(max_digits=12, decimal_places=4, default=0)
+    monto_letras = models.CharField(max_length=100, null=False)
